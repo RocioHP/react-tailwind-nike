5ac8fc1b3303485c6f7a79809d61e3d1b62d7cec
Rocío Hernández <rocioflauta0709@gmail.com>
2024-01-26 19:51:14 +0300

    Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,32 @@
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons/index.js";
+import { statistics } from "../constants"
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max.xl:p-x pt-28">
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike </span>
+          Shoes
+          </h1>
+          <p>Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.</p>
+          <Button label="Shop now" iconURL={arrowRight}/>
+
+
+          <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16"></div>
+           {statistics.map((stat, index) => (
+            <div key={index}>
+              <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+              <p className='leading-7 font-montserrat text-slate-gray'>{stat.label}</p>
+            </div>
+           ))}
+      </div>
+    </section>
   )
 }
 
