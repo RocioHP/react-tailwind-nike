d9c83a4470fc96a13950e5764f60822d5f2eff66
Rocío Hernández <rocioflauta0709@gmail.com>
2024-01-28 15:25:31 +0300

    popular products done

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,5 +1,5 @@
 
-const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg}) => {
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
   const handleClick = () => {
     if (bigShoeImg !== imgURL.bigShoe)
     {
